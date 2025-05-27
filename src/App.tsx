501e35aed32e8eb9bc4a97e238c6b8eafe7a970c
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-05-27 10:55:37 +0530

    setup done with shadecn UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className="text-blue-500">
+      <h1>Hello</h1>
+      {/* <Button variant={'outline'}>Click me</Button> */}
+    </div>
+  )
+}
+
+export default App
