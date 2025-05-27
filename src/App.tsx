c78c086e82f4621c9b5101b01867198c058d8a3e
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-05-27 11:31:53 +0530

    routing and clerk auto setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-
-import { Button } from './components/ui/button'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { PublicLayout } from "@/layouts/public-layout"
+import HomePage from "@/routes/home"
 
 const App = () => {
-  return (
-    <div className="text-blue-500">
-      <h1>Hello</h1>
-      {/* <Button variant={'outline'}>Click me</Button> */}
-    </div>
+  return(
+    <Router>
+      <Routes>
+        {/* Public Routes  */}
+        <Route element={<PublicLayout/>}>
+          <Route index element={<HomePage/>}></Route>
+        </Route>
+
+        {/* protected route  */}
+      </Routes>
+    </Router>
   )
 }
-
+ 
 export default App
