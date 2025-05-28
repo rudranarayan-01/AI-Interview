56a55659f4b59dac845aacdcc031a35f740e25f5
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-05-28 09:59:13 +0530

    Changes: Firebase Configuration with user storage

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,11 +1,15 @@
 import Footer from "@/components/footer"
 import Header from "@/components/header"
+import AuthHanlder from "@/handler/auth-handler"
+
 import { Outlet } from "react-router-dom"
 
 export const PublicLayout = () => {
   return (
     <div className="w-full">
         {/* handler to store the user data  */}
+        <AuthHanlder/>
+
         <Header/>
 
         <Outlet/>
